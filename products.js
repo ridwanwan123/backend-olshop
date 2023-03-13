25dab977a2dc3234e9f0d39440342a0ac14caeb5
Muhamad Ridwan <ridwanwan784@gmail.com>
2023-03-13 12:02:05 +0700

    Merubah format harga dan menambah product

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: 1,
     name: "iPhone 11",
-    desc: "6.1-inch display",
+    desc: "5.4-inch display",
     price: 7999599,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/01-APPLE-8DVPHAPPA-APPMHDA3ID-A-BlackRevSS_naclvt.jpg",
@@ -18,7 +18,7 @@ const products = [
   {
     id: 3,
     name: "iPhone 12 Pro Max",
-    desc: "6.4-inch mini display",
+    desc: "6.4-inch  display",
     price: 9999901,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251521/Online-shop/iphone_12_pro_pacific_blue_1_3_kiksur.webp",
@@ -26,7 +26,7 @@ const products = [
   {
     id: 4,
     name: "iPhone 13 Pro",
-    desc: "6.1-inch mini display",
+    desc: "6.1-inch  display",
     price: 10099900,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/iphone_13_pro_alpine_green_1_1_2_fldrde.webp",
@@ -34,7 +34,7 @@ const products = [
   {
     id: 5,
     name: "iPhone 13 Pro Max",
-    desc: "6.4-inch mini display",
+    desc: "6.4-inch  display",
     price: 13009990,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/apple_iphone_11_pro_max_space_grey_1_6_1_x7gbov.jpg",
@@ -42,12 +42,43 @@ const products = [
   {
     id: 6,
     name: "iPhone 14 Pro Max",
-    desc: "6.4-inch mini display",
+    desc: "6.4-inch display",
     price: 15298990,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/iphone_14_pro_deep_purple_1_vjuurk.webp",
   },
-
+  {
+    id: 7,
+    name: "Samsung S22 Ultra",
+    desc: "6.4-inch display",
+    price: 21000332,
+    image:
+      "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678683660/Online-shop/ph-galaxy-s22-ultra-s908-sm-s908ezwgphl-530760753_xaphyn.avif",
+  },
+  {
+    id: 8,
+    name: "Samsung S23 Ultra",
+    desc: "6.4-inch display",
+    price: 21000332,
+    image:
+      "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678683364/Online-shop/ph-galaxy-s22-ultra-s908-sm-s908edrgphl-530760419_jvud9g.avif",
+  },
+  {
+    id: 9,
+    name: "ROG Phone 5",
+    desc: "6.1-inch display",
+    price: 21598809,
+    image:
+      "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678683365/Online-shop/h732_fglliq.png",
+  },
+  {
+    id: 10,
+    name: "ROG Phone 6",
+    desc: "6.4-inch display",
+    price: 21699900,
+    image:
+      "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678683366/Online-shop/B46BEF43-3A3C-4A30-B73A-7A1434566EC8_sytdju.png",
+  },
 ];
 
 module.exports = products;

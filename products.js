b1890f36b8bdbdd279c5ef6d9a9d5a48e254423e
Muhamad Ridwan <ridwanwan784@gmail.com>
2023-03-13 11:49:01 +0700

    Merubah harga products

--- products.js
+++ products.js
@@ -18,32 +18,32 @@ const products = [
   {
     id: 3,
     name: "iPhone 12 Pro Max",
-    desc: "5.4-inch mini display",
-    price: 999991,
+    desc: "6.4-inch mini display",
+    price: 9999901,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251521/Online-shop/iphone_12_pro_pacific_blue_1_3_kiksur.webp",
   },
   {
     id: 4,
     name: "iPhone 13 Pro",
-    desc: "5.4-inch mini display",
-    price: 100999,
+    desc: "6.1-inch mini display",
+    price: 10099900,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/iphone_13_pro_alpine_green_1_1_2_fldrde.webp",
   },
   {
     id: 5,
     name: "iPhone 13 Pro Max",
-    desc: "5.4-inch mini display",
-    price: 1100999,
+    desc: "6.4-inch mini display",
+    price: 13009990,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/apple_iphone_11_pro_max_space_grey_1_6_1_x7gbov.jpg",
   },
   {
     id: 6,
     name: "iPhone 14 Pro Max",
-    desc: "5.4-inch mini display",
-    price: 1529899,
+    desc: "6.4-inch mini display",
+    price: 15298990,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/iphone_14_pro_deep_purple_1_vjuurk.webp",
   },

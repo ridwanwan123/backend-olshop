26ba618174deeb39a9850e056e8ea8e6995fb400
Muhamad Ridwan <ridwanwan784@gmail.com>
2023-03-13 11:39:22 +0700

    Mengganti Harga Product

--- products.js
+++ products.js
@@ -2,54 +2,48 @@ const products = [
   {
     id: 1,
     name: "iPhone 11",
-    brand: "Apple",
     desc: "6.1-inch display",
-    price: 399,
+    price: 7999599,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/01-APPLE-8DVPHAPPA-APPMHDA3ID-A-BlackRevSS_naclvt.jpg",
   },
   {
     id: 2,
     name: "iPhone 12",
-    brand: "Apple",
     desc: "5.4-inch mini display",
-    price: 499,
+    price: 8790999,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251521/Online-shop/iphone_12_purple_1_1_1_eqhtuw.webp",
   },
   {
     id: 3,
     name: "iPhone 12 Pro Max",
-    brand: "Apple",
     desc: "5.4-inch mini display",
-    price: 699,
+    price: 999991,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251521/Online-shop/iphone_12_pro_pacific_blue_1_3_kiksur.webp",
   },
   {
     id: 4,
-    name: "iPhone 13 Pro Max",
-    brand: "Apple",
+    name: "iPhone 13 Pro",
     desc: "5.4-inch mini display",
-    price: 959,
+    price: 100999,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/iphone_13_pro_alpine_green_1_1_2_fldrde.webp",
   },
   {
     id: 5,
     name: "iPhone 13 Pro Max",
-    brand: "Apple",
     desc: "5.4-inch mini display",
-    price: 1001,
+    price: 1100999,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/apple_iphone_11_pro_max_space_grey_1_6_1_x7gbov.jpg",
   },
   {
     id: 6,
     name: "iPhone 14 Pro Max",
-    brand: "Apple",
     desc: "5.4-inch mini display",
-    price: 1001,
+    price: 1529899,
     image:
       "https://res.cloudinary.com/dbvrqaynl/image/upload/v1678251520/Online-shop/iphone_14_pro_deep_purple_1_vjuurk.webp",
   },
